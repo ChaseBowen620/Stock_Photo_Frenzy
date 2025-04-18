1cc0c49624ac52252d60dcb76bc8811a56d539d7
chasebowen620 <chase.c.bowen@gmail.com>
2025-04-18 17:21:03 -0600

    updated ui materials

--- Front_End_SPF/src/App.tsx
+++ Front_End_SPF/src/App.tsx
@@ -1,13 +1,32 @@
 import { Routes, Route } from 'react-router-dom'
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material'
 import Game from './components/Game'
 import Home from './components/Home'
 
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  },
+});
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/game" element={<Game />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </ThemeProvider>
   )
 }
 
